5efdee39f282be29d163b84ac532ed4d0c644c21
Eduardo Soares <edurbs@gmail.com>
2023-12-08 18:57:20 -0300

    arrays

--- AreaCalcutator.java
+++ AreaCalcutator.java
@@ -4,6 +4,19 @@ public class AreaCalcutator{
     public static void main (String[] args){
         System.out.println(GeometricCalc.circle(10));
         System.out.println(GeometricCalc.square(10));
+
+        int[] notas1 = new int[5]; // 0,0,0,0
+        notas1 = new int[]{1,2,3,4,5};
+
+        int[] notas2 = {1,2,3,4,5};
+
+        int[] notas3;        
+        notas3 = new int[]{1,2,3,4,5};
+
+        int[] notas4 = new int[]{1,2,3,4,5};
+
+        System.out.println(notas1);
+
     }
 
     public class GeometricCalc{
